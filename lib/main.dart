0608b10a1e38fac9bf0240d8aba6783cbac35756
Jakub Zawada <JakubZawadaa3@gmail.com>
2022-11-13 15:41:23 +0100

    Install FirebaseAuth

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -20,7 +21,34 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const RootPage(),
     );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          final user = snapshot.data;
+          if (user == null) {
+            return const Scaffold(
+              body: Center(
+                child: Text('Jesteś niezalogowany'),
+              ),
+            );
+          }
+          return Scaffold(
+            body: Center(
+              child: Text('Jesteś zalogowany jako ${user.email}'),
+            ),
+          );
+        });
   }
 }
