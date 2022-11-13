0283631dfaf85a2e26ab3b8edb1d07e6b0b68418
Jakub Zawada <JakubZawadaa3@gmail.com>
2022-11-13 15:47:45 +0100

    Organize files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:this_is_your_training/app/home/home_page.dart';
+import 'package:this_is_your_training/app/login/login_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -38,17 +40,9 @@ class RootPage extends StatelessWidget {
         builder: (context, snapshot) {
           final user = snapshot.data;
           if (user == null) {
-            return const Scaffold(
-              body: Center(
-                child: Text('Jesteś niezalogowany'),
-              ),
-            );
+            return const LoginPage();
           }
-          return Scaffold(
-            body: Center(
-              child: Text('Jesteś zalogowany jako ${user.email}'),
-            ),
-          );
+          return HomePage(user: user);
         });
   }
 }
